f480ccf42fb08948294860f822ae5be23c79fb7f
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-03-26 20:16:33 -0300

    Questão 16 função alterada

--- Q16.py
+++ Q16.py
@@ -1,8 +1,8 @@
 tamanho_parede = float(input('Digite o tamanho em metros² da parede a ser pintada: '))
 litros = tamanho_parede/3
-qtd_latas = tamanho_parede/(54)
+qtd_latas = round(tamanho_parede/54)
 preco_total = qtd_latas * 80
 print(f' A quantidade de latas será: {qtd_latas:.2f} Latas')
-print(f' Total em Litros {litros:.2f}')
+print(f' Total em Litros {litros:.2f} L')
 print(f' O preço da lata fracionada será de : R${preco_total:.2f}')
 
