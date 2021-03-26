2f8d893d3ea45b77a96bbd6492740260048d6008
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-03-25 21:52:43 -0300

    Questão 8

---
+++ Q8.py
@@ -0,0 +1,4 @@
+valor_hora = float(input('Digite o valor da sua hora de trabalho em R$: '))
+qtd_horas = int(input('Quantas horas trabalhou no mês? :'))
+salario_horista = valor_hora * qtd_horas
+print(f'Seu salário foi de: R${salario_horista}')
