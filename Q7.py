cffa34227d096f8b68b3ba393387107f92ae2b93
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-03-25 21:38:17 -0300

    Questão 7

---
+++ Q7.py
@@ -0,0 +1,4 @@
+import math 
+lado = float(input('Digite o lado do quadrado para calcular sua área: '))
+area_quadrado = math.pow(lado,2)
+print(f'A área do quadrado é {area_quadrado} e seu dobro é: {area_quadrado*2}')
