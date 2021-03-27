c363f2ed9ba60d657b47d2198c6a978391b53c18
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-03-27 17:57:08 -0300

    Questão 1

---
+++ Lista2/Q1.py
@@ -0,0 +1,9 @@
+''''Faça um Programa que peça dois números e imprima o maior deles.'''
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+if num1 > num2:
+    print(f' {num1} é maior que {num2}')
+elif num2 > num1:
+    print(f' {num2} é maior que {num1}')
+else:
+    print(f'Os números: {num1} e {num2} são iguais ')
