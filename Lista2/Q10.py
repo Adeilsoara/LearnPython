a30be61b7dacfb2f5ba5b2b3bdb2ab6b068f3d26
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-03-30 19:50:46 -0300

    Questão 10

---
+++ Lista2/Q10.py
@@ -0,0 +1,17 @@
+
+'''Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou
+V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!"
+ou "Valor Inválido!", conforme o caso. '''
+
+turno = input('Em qual turno você estuda?'
+              '( M-matutino '
+              ' V-vespertino '
+              ' N-noturno ): ').lower()
+if turno == 'm':
+    print('Bom dia!')
+elif turno == 'v':
+    print('Boa tarde!')
+elif turno == 'n':
+    print('Boa noite!')
+else:
+    print('Informação Inválida!')
