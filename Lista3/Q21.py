c2563ca804478c0da7ceb31f0166bdf9db229749
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-04-23 19:42:05 -0300

    Questão 21

---
+++ Lista3/Q21.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
+Um número primo é aquele que é divisível somente por ele mesmo e por 1
+'''
+'''cont = 2
+contador = 0
+num = int(input('Informe um número: '))
+if num <= 1:
+    print(f'{num} Não é primo')
+else:
+    while num % cont == 0:
+        print('não é primo')'''
+
+
