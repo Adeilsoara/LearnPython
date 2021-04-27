3bcbb4562ea677d346e193ac2abc00695cd43733
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-04-27 20:30:43 -0300

    Questão 28

--- Lista3/Q28.py
+++ Lista3/Q28.py
@@ -6,7 +6,7 @@ O usuário deverá informar a quantidade de CDs e o valor para em cada um.
 valor_cds = []
 numero_cds = int(input("Quantos CDs, deseja informar? : "))
 for colecao in range(1,numero_cds+1):
-    valor_cds.append(int(input(f'Informe o valor do CD {colecao}')))
+    valor_cds.append(int(input(f'Informe o valor do {colecao}ª CD ')))
 valor_total = numero_cds * (sum(valor_cds))
 valor_medio = sum(valor_cds) / numero_cds
 print(f'O valor total da sua coleção de CDs é de: {valor_total}')
