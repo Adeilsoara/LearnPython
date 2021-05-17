906b30adf9913991c3d6b6eb5f2814eb5afa796f
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-17 20:58:02 -0300

    Lib PyWhatkit - Programa converter caracteres em texto escrito e meme do Celcius

---
+++ BrincandoComPython/meme.py
@@ -0,0 +1,4 @@
+celsius = float(input('Informe a temperatura em graus celsius °: '))
+fahren = (celsius * 9/5) + 32
+somaCelsius = fahren + fahren
+print(f' A temperatura em celcius é: {somaCelsius} °F')
---
+++ BrincandoComPython/texto_escrita_mao.py
@@ -0,0 +1,3 @@
+import  pywhatkit as kit
+
+kit.text_to_handwriting("Sua mensagem",r"C:\nomeQualquer.png")
