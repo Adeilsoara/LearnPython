77d2f5b76d636676d9f23711c2091d1e9c93fa5c
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-17 19:59:23 -0300

    Lib pywhatkit - Alterando extensão do arquivo

--- BrincandoComPython/libwhatkit..py
+++
@@ -1,3 +0,0 @@
-import  pywhatkit as kit
-
-kit.sendwhatmsg(f"+55{8896867385}", "Ei mah testando", 19,52)
---
+++ BrincandoComPython/libwhatkit.py
@@ -0,0 +1,3 @@
+import  pywhatkit as kit
+
+kit.sendwhatmsg(f"+55{0}", "Ei mah testando", 19,52)
