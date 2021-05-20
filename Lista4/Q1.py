f8c9a55e355674604093bdc395abbdf7948513cd
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-20 20:44:54 -0300

    Questão 01

---
+++ Lista4/Q1.py
@@ -0,0 +1,8 @@
+'''
+Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+'''
+numeros = []
+for i in range(1,6):
+    num = float(input(f'Digite o {i}ª número: '))
+    numeros.append(num)
+print(f'Os números do vetor são: {numeros}')
