e06b517c7f261511b519f36e674269086dbed457
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 19:40:01 -0300

    Questão 04

---
+++ Lista4/Q04.py
@@ -0,0 +1,14 @@
+'''
+Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
+ Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três ve'''
+vogais = ['a','e','i','o','u']
+consoantes = []
+cont = 0
+for i in range(4):
+    consoantes.append(input(f'Digite o {i+1}ª caracter: '))
+    conso = consoantes[i]
+    if conso not in vogais:
+        cont +=1
+print(f'A quantidade de consoantes é: {cont}')
