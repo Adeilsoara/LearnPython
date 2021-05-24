ab5bc7008305457984945c4a9e94b88ec3d6aace
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-24 20:11:10 -0300

    Questão 05

--- Lista4/Q04.py
+++ Lista4/Q04.py
@@ -1,7 +1,7 @@
 '''
 Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas.
 Imprima as consoantes.
- Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três ve'''
+ '''
 vogais = ['a','e','i','o','u']
 consoantes = []
 cont = 0
--- Lista4/Q05.py
+++ Lista4/Q05.py
@@ -1,7 +1,16 @@
 '''
 Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
+Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 '''
 numeros20 = []
+numerosPar = []
+numerosImpar = []
 for i in range(20):
     numeros20.append(int(input(f'Informe o {i + 1}ª número, faltam {20 - i} números: ')))
+    if numeros20[i] % 2 == 0:
+        numerosPar.append(numeros20[i])
+    else:
+        numerosImpar.append(numeros20[i])
 print(f'Os números armazenados foram: {numeros20}')
+print(f'Os números Pares armazenados foram: {numerosPar}')
+print(f'Os números Ímpares armazenados foram: {numerosImpar}')
