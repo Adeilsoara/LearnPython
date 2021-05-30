87ede9b6a15da37ea63c56f0a69b1fe4af64eab8
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-30 16:32:10 -0300

    Questão 09

---
+++ Lista4/Q09.py
@@ -0,0 +1,10 @@
+'''
+Faça um Programa que leia um vetor A com 10 números inteiros,
+calcule e mostre a soma dos quadrados dos elementos do vetor.
+'''
+vetorA = []
+for i in range (3):
+    numeros = int(input(f'Informe o {i+1}ª: '))
+    vetorA.append(numeros**2)
+print(f'Os elementos ao quadrado do vetor informado é: {vetorA}')
+print(f'A soma dos elementos informados é: {sum(vetorA)}')
