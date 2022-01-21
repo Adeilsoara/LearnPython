bebd5173b7633fab4ff392a9d1676d650bb9efc1
Adeilson Aragão <adeilsonaragao@hotmail.com>
2022-01-21 10:27:59 -0300

    Questão 03

---
+++ Lista6/Q3.py
@@ -0,0 +1,11 @@
+'''Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+
+    F
+    U
+    L
+    A
+    N
+    O'''
+nome = input('Digite seu nome:')
+for i in list(nome):
+    print(i)
