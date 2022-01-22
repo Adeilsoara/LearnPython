b3e60e29a29c02d9da573f95335bb830aa926db0
Adeilson Aragão <adeilsonaragao@hotmail.com>
2022-01-22 14:15:34 -0300

    Questão 05

---
+++ Lista6/Q5.py
@@ -0,0 +1,11 @@
+'''Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+
+    FULANO
+    FULAN
+    FULA
+    FUL
+    FU
+    F'''
+nome = input('Digite seu nome: ')
+for i in range(0, len(nome)+1):
+    print(nome[i:])
