9fea585b66bde0b1722ecdddc2ae21e3088b9620
Adeilson Aragão <adeilsonaragao@hotmail.com>
2022-01-25 16:06:48 -0300

    Questão 10

---
+++ Lista6/Q10.py
@@ -0,0 +1,6 @@
+'''
+Número por extenso. Escreva um programa que solicite ao usuário a digitação de um número até 99 e imprima-o na tela por extenso.
+'''
+numeroPorExtenso = int(input('Digite um número: '))
+
+#Não Feita por Motivos de Preguiça
