246041d36bdf72a0ca02a050feaf8ef8efc8f448
Adeilsoara <adeilsonaragao@hotmail.com>
2022-02-20 15:46:04 -0300

    Conversor de moeda com API- Usando conceitos de classes

--- ConversorDeMoedas/conversormoedas.py
+++ ConversorDeMoedas/conversormoedas.py
@@ -1,12 +1,14 @@
 import requests
 
-requisicao = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
-resposta = requisicao.json()
+def converter():
+    requisicao = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+    resposta = requisicao.json()
+    cotacao = resposta['USDBRL']['high']
+    return cotacao
 
-
-cotacao = resposta['USDBRL']['high']
 print('*'*10 +'CONVERSOR DE MOEDA USD -> BRL' + '*'*10)
 real = float(input('Digite o valor em Dólar: '))
-conversao = round((float(cotacao) * real), 2)
+conversao = round((float(converter()) * real), 2)
 
 print(f'USD {conversao}')
+
