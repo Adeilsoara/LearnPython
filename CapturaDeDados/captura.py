aa062319708b45a293aea794059498c5203c0ae3
Adeilsoara <adeilsonaragao@hotmail.com>
2022-02-25 10:59:16 -0300

    Web scraping - Extraindo links do meu repositório

--- CapturaDeDados/captura.py
+++ CapturaDeDados/captura.py
@@ -2,13 +2,17 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 
-wiki = "https://pt.wikipedia.org/wiki/Lista_de_capitais_do_Brasil"
+repo = "https://github.com/Adeilsoara?tab=repositories"
 
-page = urllib.request.urlopen(wiki)
+page = urllib.request.urlopen(repo)
 
 soup = BeautifulSoup(page, 'html5lib')
 # print(soup.title)
 # print(soup.title.string)
-list_item = soup.find('li', attrs={'class': 'toclevel-1 tocsection-5'})
-name = list_item.text.strip()
-print(name)
+todos_links = soup.find_all('a')
+for link in todos_links:
+    print(link.get('href'))
+    
+# list_item = soup.find('a', attrs={'itemprop': 'name codeRepository'})
+# name = list_item.text.strip()
+#print(name)
