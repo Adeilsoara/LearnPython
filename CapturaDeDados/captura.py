4c77490b581ecb270dcb3519166725e6acb7f366
Adeilsoara <adeilsonaragao@hotmail.com>
2022-02-25 10:40:30 -0300

    Web scraping - Extraindo Capitais da Wiki

--- CapturaDeDados/captura.py
+++ CapturaDeDados/captura.py
@@ -7,7 +7,8 @@ wiki = "https://pt.wikipedia.org/wiki/Lista_de_capitais_do_Brasil"
 page = urllib.request.urlopen(wiki)
 
 soup = BeautifulSoup(page, 'html5lib')
-
-list_item = soup.find('li', attrs={'class': 'toclevel-2 tocsection-26'})
+# print(soup.title)
+# print(soup.title.string)
+list_item = soup.find('li', attrs={'class': 'toclevel-1 tocsection-5'})
 name = list_item.text.strip()
 print(name)
