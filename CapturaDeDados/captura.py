240339f7be111d06ed72155aadc92da59970c74d
Adeilsoara <adeilsonaragao@hotmail.com>
2022-02-26 19:37:23 -0300

    Web scraping - Extraindo links do meu repositório

--- CapturaDeDados/captura.py
+++ CapturaDeDados/captura.py
@@ -12,7 +12,8 @@ soup = BeautifulSoup(page, 'html5lib')
 todos_links = soup.find_all('a')
 for link in todos_links:
     print(link.get('href'))
-    
+
+
 # list_item = soup.find('a', attrs={'itemprop': 'name codeRepository'})
 # name = list_item.text.strip()
 #print(name)
